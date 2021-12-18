b8d3da225a381caceac2cd5ab2df06b184483a4a
Asir053 <asirbillah@gmail.com>
2021-12-18 20:56:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { todos: [] };
+
+  async componentDidMount() {
+    let res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    this.setState({ todos: res.data });
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        {this.state.todos.length > 0 ? (
+          <div>
+            <ul className='list-group'>
+              {this.state.todos.map((todo) => (
+                <li className='list-group-item d-flex justify-content-between align-items-center'>
+                  {todo.title}
+                  <span>
+                    <input type='checkbox' checked={todo.completed} />
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div>
+            <span>Loading...</span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
